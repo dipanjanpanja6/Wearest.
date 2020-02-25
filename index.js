cefa026f9c9d573a8c863bde6a4d6eefc1b08f57
D1p4Nj4Np4Nj4 <dipanjanpanja6@noreply.github.com>
2020-02-25 20:54:14 +0530

    update icon

--- index.js
+++ index.js
@@ -4,145 +4,219 @@ var temp = document.getElementById("deg-1");
 var desc = document.getElementById("desc");
 var date = document.getElementById("date");
 var addr = document.getElementById("addr");
+var main_img = document.getElementById("main_img");
+var main_icon = document.getElementById("main_icon");
 
 var x = document.querySelectorAll("H6");
 
 var day = document.getElementsByClassName("day-bar");
 var temp_all = document.getElementsByClassName("deg");
+var icon = document.getElementsByClassName("icon_s");
 
 
-button.addEventListener("click", function() {
+
+fetch(
+  "http://api.ipstack.com/check?access_key=1093d4911c69fc7a8497b7d78fcc1621&format=1"
+  )
+  .then(response => response.json())
+  .then(data => {
+    var geo = data["city"];
+    console.log(geo);
+    weathercall(geo);
+  });
+
+function weathercall(geo) {
   fetch(
     "https://api.openweathermap.org/data/2.5/forecast?q=" +
-      input.value +
+      geo +
       "&appid=82eb8bc7304fb87bdbfbae435106fe06&cnt=7&units=metric"
   )
     .then(response => response.json())
     .then(data => {
-    
-      temp_all[0].innerText = Math.floor(data["list"][1]["main"]["temp"]);
-      temp_all[1].innerText = Math.floor(data["list"][2]["main"]["temp"]);
-      temp_all[2].innerText = Math.floor(data["list"][3]["main"]["temp"]);
-      temp_all[3].innerText = Math.floor(data["list"][4]["main"]["temp"]);
-      temp_all[4].innerText = Math.floor(data["list"][5]["main"]["temp"]);
-      temp_all[5].innerText = Math.floor(data["list"][6]["main"]["temp"]);
-      temp.innerText = Math.floor(data["list"][0]["main"]["temp"]);
-
-var addr_value = data['city']['name'];
-var country_value = data['city']['country'];
 
 
-var des0 = data['list'][0]['weather'][0]['main'];
-var des1 = data['list'][1]['weather'][0]['main'];
-var des2 = data['list'][2]['weather'][0]['main'];
-var des3 = data['list'][3]['weather'][0]['main'];
-var des4 = data['list'][4]['weather'][0]['main'];
-var des5 = data['list'][5]['weather'][0]['main'];
-var des6 = data['list'][6]['weather'][0]['main'];
+var cur_icon=[];
+for (var i = 0; i < 7; i++) {
+  cur_icon[i] = data["list"][i]["weather"][0]["icon"];
+}
+main_icon.src="icon/"+cur_icon[0]+".png";
+for( var i=0; i<6; i++){
+  icon[i].src="icon/"+cur_icon[i+1]+".png";
 
-
-
-var timestamp0 = data['list'][0]['dt'];
-var timestamp1 = data['list'][1]['dt'];
-var timestamp2 = data['list'][2]['dt'];
-var timestamp3 = data['list'][3]['dt'];
-var timestamp4 = data['list'][4]['dt'];
-var timestamp5 = data['list'][5]['dt'];
-var timestamp6 = data['list'][6]['dt'];
-
-    
-date.innerText = timeConverter(timestamp0);    
-day[0].innerText = dayConverter(timestamp1);
-day[1].innerText = dayConverter(timestamp2);
-day[2].innerText = dayConverter(timestamp3);
-day[3].innerText = dayConverter(timestamp4);
-day[4].innerText = dayConverter(timestamp5);
-day[5].innerText = dayConverter(timestamp6);
-
-
-
-
-    description(des0,des1,des2,des3,des4,des5,des6);
-    address(addr_value,country_value);
-    })
-    .catch(err => alert("Enter Wrong City Name"));
-});
-
-// http://openweathermap.org/img/w/10n.png
-
-function timeConverter(UNIX_timestamp) {
-  var a = new Date(UNIX_timestamp * 1000);
-  var days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  var months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "Jun",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-  ];
-  var year = a.getFullYear();
-  var month = months[a.getMonth()];
-  var day = days[a.getDay()];
-  var date = a.getDate();
-  var hour = a.getHours();
-  var min = a.getMinutes();
-  var sec = a.getSeconds();
-  var time =
-    //   day :day,
-    //   date:date,
-    //   month:month,
-    //   year:year
-
-    day +
-    ", " +
-    date +
-    " " +
-    month +
-    " " +
-    year +
-    " " +
-    hour +
-    ":" +
-    min
-  ;
-  return time;
 }
 
-function dayConverter(UNIX) {
-    var a = new Date(UNIX * 1000);
-    var x = a.toString().split(' ')[0];
-    return x;
+
+
+var cur_img=
+
+   data["list"][0]["weather"][0]["icon"];
+
+main_img.src="icon/big/"+cur_img+".png";
+
+
+
+
+
+
+
+
+
+
+      var cur_temp = [];
+      for (var i = 0; i < 7; i++) {
+        cur_temp[i] = Math.floor(data["list"][i]["main"]["temp"]);
+      }
+      console.log(cur_temp);
+      getinfo(cur_temp);
+
+
+
+      
+       var timestamp = [];
+        for (var i = 0; i < 7; i++) {
+          timestamp[i] = data["list"][i]["dt"];
+        }
+        console.log(timestamp);
+        for (var i = 0; i < 6; i++) {
+          day[i].innerText = dayConverter(timestamp[i+1]);
+        }
+        date.innerText = timeConverter(timestamp[0]);
+
+
+
+        var addr_value = data["city"]["name"];
+        var country_value = data["city"]["country"];
+        address(addr_value, country_value);
+
+
+
+
+        var des = [];
+        for (var i = 0; i < 7; i++) {
+          des[i] = data["list"][i]["weather"][0]["main"];
+        }
+        description(des);
+
+    });
+  }
+  
+  function getinfo(info) {
+    temp.innerText = info[0];
+    for (var i = 0; i < 6; i++) {
+      temp_all[i].innerText = info[i + 1];
+    }
   }
 
 
-function address(a,b){
-    addr.innerText = a+", "+b;
-}
-function description(a,b,c,d,e,f,g){
-    x[0].innerHTML = b;
-    x[1].innerHTML = c;
-    x[2].innerHTML = d;
-    x[3].innerHTML = e;
-    x[4].innerHTML = f;
-    x[5].innerHTML = g;
-    desc.innerText = a;
-}
+
+  
+  
+
+  button.addEventListener("click", function() {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+        input.value +
+        "&appid=82eb8bc7304fb87bdbfbae435106fe06&cnt=7&units=metric"
+    )
+      .then(response => response.json())
+      .then(data => {
+        var temp_all = [];
+        for (var i = 0; i < 7; i++) {
+          temp_all[i] = Math.floor(data["list"][i]["main"]["temp"]);
+        }
+        console.log(temp_all);
+        getinfo(temp_all);
+
+        
+        var des = [];
+        for (var i = 0; i < 7; i++) {
+          des[i] = data["list"][i]["weather"][0]["main"];
+        }
+        description(des);
 
 
 
+        
+        var timestamp = [];
+        for (var i = 0; i < 7; i++) {
+          timestamp[i] = data["list"][i]["dt"];
+        }
+        for (var i = 0; i < 6; i++) {
+          day[i].innerText = dayConverter(timestamp[i + 1]);
+        }
+        date.innerText = timeConverter(timestamp[0]);
+        
+        
+        var addr_value = data["city"]["name"];
+        var country_value = data["city"]["country"];
+        address(addr_value, country_value);
 
+
+      })
+      .catch(err => alert("Enter Wrong City Name"));
+  });
+
+  // http://openweathermap.org/img/w/10n.png
+
+  function timeConverter(UNIX_timestamp) {
+    var a = new Date(UNIX_timestamp * 1000);
+    var days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+    var months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "Jun",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var day = days[a.getDay()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var time =
+      //   day :day,
+      //   date:date,
+      //   month:month,
+      //   year:year
+
+      day + ", " + date + " " + month + " " + year + " " + hour + ":" + min;
+    return time;
+  }
+
+  function dayConverter(UNIX) {
+    var a = new Date(UNIX * 1000);
+    var x = a.toString().split(" ")[0];
+    return x;
+  }
+
+  function address(a, b) {
+    addr.innerText = a + ", " + b;
+  }
+  function description(des) {
+    for (var i = 0; i < 6; i++) {
+      x[i].innerHTML = des[i + 1];
+    }
+    desc.innerText = des[0];
+    // x[1].innerHTML = c;
+    // x[2].innerHTML = d;
+    // x[3].innerHTML = e;
+    // x[4].innerHTML = f;
+    // x[5].innerHTML = g;
+  }
